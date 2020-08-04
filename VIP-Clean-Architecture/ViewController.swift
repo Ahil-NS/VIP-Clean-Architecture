58cb541c246716401683ab4c158ab2f83c46044f
Ahil <sinnaiah.ahilendran@accenture.com>
2020-08-05 00:04:58 +0800

    Initial Commit

---
+++ VIP-Clean-Architecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VIP-Clean-Architecture
+//
+//  Created by Ahil Ahilendran on 5/8/20.
+//  Copyright © 2020 Ahil Ahilendran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
