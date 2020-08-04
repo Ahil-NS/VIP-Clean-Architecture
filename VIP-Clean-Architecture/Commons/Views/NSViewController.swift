7c6d1f46d6503c06689d6a26b1612ccea929d0e5
Ahil <sinnaiah.ahilendran@accenture.com>
2020-08-05 01:02:31 +0800

    setup base layers

---
+++ VIP-Clean-Architecture/Commons/Views/NSViewController.swift
@@ -0,0 +1,9 @@
+//
+//  NSViewController.swift
+//  VIP-Clean-Architecture
+//
+//  Created by Ahil Ahilendran on 5/8/20.
+//  Copyright © 2020 Ahil Ahilendran. All rights reserved.
+//
+
+import Foundation
