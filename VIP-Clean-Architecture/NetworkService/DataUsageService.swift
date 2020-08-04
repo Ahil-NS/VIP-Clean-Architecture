b67f0b951952ba0f6b7d30883f0c7d37b636a768
Ahil <sinnaiah.ahilendran@accenture.com>
2020-08-05 03:02:32 +0800

    implement GET Mobile data usage api service

--- VIP-Clean-Architecture/NetworkService/DataUsageService.swift
+++ VIP-Clean-Architecture/NetworkService/DataUsageService.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+import Moya
+
+public enum DataUsageService {
+    case getDataUsage(offset: Int, limit: Int = 10, resourceId: String)
+}
+
+extension DataUsageService: TargetType {
+    public var baseURL: URL {
+        switch self {
+        case .getDataUsage(let offset, let limit, let resourceId):
+            //TODOA
+            return URL(string: "https://data.gov.sg/api/action/datastore_search?offset=\(offset)&resource_id=\(resourceId)&limit=\(limit)")!
+        }
+    }
+    
+    public var path: String {
+        switch self {
+        case .getDataUsage : return ""
+        }
+    }
+    
+    public var method: Moya.Method {
+        switch self {
+        case .getDataUsage: return .get
+            
+        }
+    }
+    
+    public var sampleData: Data {
+        return Data()
+    }
+    
+    public var task: Task {
+        switch self {
+        case .getDataUsage:
+             return .requestPlain
+        }
+    }
+    
+    public var headers: [String: String]? {
+        return ["Content-Type": "application/json"]
+    }
+    
+}
+
---
+++ VIP-Clean-Architecture/NetworkService/MicroServicePaths.swift
@@ -0,0 +1,13 @@
+//
+//  MicroServicePaths.swift
+//  VIP-Clean-Architecture
+//
+//  Created by Ahil Ahilendran on 5/8/20.
+//  Copyright © 2020 Ahil Ahilendran. All rights reserved.
+//
+
+import Foundation
+
+struct MicroServicePaths {
+    static let dataUsage = "/api/action/datastore_search?"
+}
